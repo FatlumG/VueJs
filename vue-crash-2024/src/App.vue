30f6986c96c9461c9497dcdffe8c43cc6548060f
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-03-11 17:28:37 +0100

    Learned v-on

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -8,6 +8,17 @@ export default {
       link: "https://google.com",
     };
   },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
 };
 </script>
 
@@ -24,6 +35,9 @@ export default {
 
   <a v-bind:href="link">Click for google</a>
   <a :href="link">Click for google</a>
+  <br>
+  <!-- <button v-on:click="toggleStatus">Change status</button> -->
+  <button @click="toggleStatus">Change status</button>
 </template>
 
 <style scoped></style>
